e9404849588fd8b910612556a54787759d0133b6
v.dybysov <v@dybysov.ru>
2024-03-03 16:26:04 +0500

    redundant annotation

--- backend/common/src/main/java/chatweb/controller/ApiControllerHelper.java
+++ backend/common/src/main/java/chatweb/controller/ApiControllerHelper.java
@@ -6,20 +6,15 @@ import chatweb.model.api.ApiError;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 public interface ApiControllerHelper {
     @ExceptionHandler(ApiErrorException.class)
-    @ResponseBody
     default ApiError handleApiError(HttpServletResponse response, ApiErrorException ex) {
         response.setStatus(ex.getApiError().getStatusCode());
         return ex.getApiError();
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     default ApiError handleUnauthorized(HttpServletResponse response) {
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         return new ApiError(HttpStatus.UNAUTHORIZED, "unauthorized");
