9a64ac5fc0f35836b9ca938b3998486cea4531ee
v.dybysov <v@dybysov.ru>
2024-08-01 11:30:42 +0200

    simplified redis & db env vars

--- backend/common/src/main/java/chatweb/configuration/RedisConfiguration.java
+++ backend/common/src/main/java/chatweb/configuration/RedisConfiguration.java
@@ -1,28 +1,17 @@
 package chatweb.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 @Configuration
-@ConditionalOnProperty("redis.url")
+@ConditionalOnProperty("spring.data.redis.url")
 public class RedisConfiguration {
-    @Bean
-    public RedisConnectionFactory lettuceConnectionFactory(@Value("${redis.url}") String redisUrl) throws URISyntaxException {
-        URI uri = new URI(redisUrl);
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(uri.getHost(), uri.getPort()));
-    }
 
     @Bean
-    public RedisTemplate<String, Integer> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Integer> redisTemplate(LettuceConnectionFactory connectionFactory) {
         RedisTemplate<String, Integer> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         return template;
